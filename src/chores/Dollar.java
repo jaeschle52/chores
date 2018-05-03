379a75b1efaf2ec5ad637b1429b89415fa538d94
Jeremiah-laptop <jaeschle52@gmail.com>
2018-05-03 08:11:08 +0100

    Last from laptop

--- src/chores/Dollar.java
+++ src/chores/Dollar.java
@@ -1,10 +1,13 @@
 package chores;
 
+import java.util.Currency;
+
 public class Dollar {
     int dollar;
     int cents;
     Currency symbol;
     
     public String printValue() {
+    	return new String(symbol.getSymbol() + dollar + "." + cents);
     }
 }
