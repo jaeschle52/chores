e129f47b190e271725c1bcde875d38a47c3dd6a3
Jeremiah-laptop <jaeschle52@gmail.com>
2018-05-03 08:35:39 +0100

    More Dollar methods

--- src/chores/Dollar.java
+++ src/chores/Dollar.java
@@ -10,4 +10,35 @@ public class Dollar {
     public String printValue() {
     	return new String(symbol.getSymbol() + dollar + "." + cents);
     }
+    
+    public Dollar(String value) {
+    	String p1 = value.substring(0, 2);
+    	String p2 = value.substring(3, 4);
+    }
+    
+    public void addMoney(Dollar d2) {
+    	int cent = cents + d2.cents;
+    	if (cent>=100) {
+    		cent =- 100;
+    		dollar =+ 1;
+    	} 
+    	dollar =+ d2.dollar;
+    }
+    
+    public void subtractMoney(Dollar d2) {
+    	int cent = cents - d2.cents;
+    	if (cent < 0) {
+    		cents =+ 100;
+    		dollar =- 1;
+    	}
+    	dollar =- d2.dollar;
+    }
+    
+    public int getDollar() {
+    	return dollar;
+    }
+    
+    public int getCent() {
+    	return cents;
+    }
 }
